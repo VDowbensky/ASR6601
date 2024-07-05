0543c380e6891e31cb554bdbbdd5d5e309461a9d
VDowbensky <harbinger@ukr.net>
2024-07-05 18:54:52 +0300

    AES

--- Ra-08/tremo_testFW/AES/aes.c
+++ Ra-08/tremo_testFW/AES/aes.c
@@ -25,17 +25,20 @@ uint8_t aes_init(uint8_t *key,uint8_t keymod,uint8_t mode,uint8_t *IVorNonce)
     //*(uint32_t*)0x4003200c &= 0xfffffffc;
 		SAE->SAEGPR0 &= 0xfffffffc;
     //if (((mode == 1) || (mode == 2)) || (mode == 3)) 
-	if (((mode == AES_CBC_MODE) || (mode == AES_OFB_MODE)) || (mode == AES_CTR_MODE))
-	{
-      memcpy((void*)0x40031610,IVorNonce,0x10); //IV
+		if (((mode == AES_CBC_MODE) || (mode == AES_OFB_MODE)) || (mode == AES_CTR_MODE))
+		{
+      //memcpy((void*)0x40031610,IVorNonce,0x10); //IV
+			memcpy((void*)AES_IVADR,IVorNonce,0x10); //IV
     }
     //if (mode == 3) 
 	if (mode == AES_CTR_MODE) 
 	{
-      *(uint32_t*)0x40031600 = 0;
+      //*(uint32_t*)0x40031600 = 0;
+			*(uint32_t*)AES_ONEADR = 0;
       *(uint32_t*)0x40031604 = 0;
       *(uint32_t*)0x40031608 = 0;
       *(uint32_t*)0x4003160c = 0x1000000;
+		
     }
     return AESSuccess; //0;
   }
@@ -62,15 +65,22 @@ uint8_t aes_crypto(uint8_t *in,uint16_t inLen,uint8_t En_De,uint8_t *out)
   //  Ram4003200c &= 0xfffffffc;
   //  Ram4003200c |= 1;
   //}
-  *(uint32_t*)0x4003200c &= 0xfffffffc;
-  if (En_De == AES_DEC_MODE) *(uint32_t*)0x4003200c |= 1;
-  
+  //*(uint32_t*)0x4003200c &= 0xfffffffc;
+	AESCR &= ~AES_MODE_MASK; //0xfffffffc;
+  //if (En_De == AES_DEC_MODE) *(uint32_t*)0x4003200c |= 1;
+	if (En_De == AES_DEC_MODE) AESCR |= AES_MODE_DEC; //1;
   for (i = inLen; i != 0; i -= 0x10) 
   {
-    memcpy((void*)0x40031000,in_ptr,0x10); //block to encrypt/decrypt
-    *(uint32_t*)0x4003200c |= 0x80;
-    while ((*(uint32_t*)0x4003200c & 0x80) != 0); //wait for completing
-    memcpy(out_ptr,(void*)0x40031020,0x10); //store result
+    //memcpy((void*)0x40031000,in_ptr,0x10); //block to encrypt/decrypt
+		memcpy((void*)AES_XADR,in_ptr,0x10); //block to encrypt/decrypt
+    //*(uint32_t*)0x4003200c |= 0x80;
+		//SAE->SAEGPR0 |= 0x80;
+		AESCR |= AES_RUN;
+    //while ((SAE->SAEGPR0 & 0x80) != 0); //wait for completing
+		while(AESCR & AES_RUN);
+		//while ((*(uint32_t*)0x4003200c & 0x80) != 0); //wait for completing
+    //memcpy(out_ptr,(void*)0x40031020,0x10); //store result
+		memcpy(out_ptr,(void*)AES_YADR,0x10); //store result
     in_ptr = in_ptr + 0x10; //next block
     out_ptr = out_ptr + 0x10;
   }
@@ -79,9 +89,12 @@ uint8_t aes_crypto(uint8_t *in,uint16_t inLen,uint8_t En_De,uint8_t *out)
 
 uint8_t aes_close(void)
 {
-  *(uint32_t*)0x40032004 |= 1;
-  while ((*(uint32_t*)0x40032004 & 1) != 0);
-  *(uint32_t*)0x40032004 &= 0xfffffff7;
+  //*(uint32_t*)0x40032004 |= 1;
+	SAE->SAEHCR |= RAMCLR;
+  //while ((*(uint32_t*)0x40032004 & 1) != 0);
+	while(SAE->SAEHCR & RAMCLR);
+  //*(uint32_t*)0x40032004 &= 0xfffffff7;
+	SAE->SAEHCR &= ~SAECLKEN; 
   return AESSuccess; //0;
 }
 

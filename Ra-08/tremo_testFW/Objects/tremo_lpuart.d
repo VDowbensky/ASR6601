6918afea9c317a0481838efe3341570beecd73a9
VDowbensky <harbinger@ukr.net>
2024-07-30 14:34:09 +0300

    0

--- Ra-08/tremo_testFW/Objects/tremo_lpuart.d
+++ Ra-08/tremo_testFW/Objects/tremo_lpuart.d
@@ -1,10 +1,3 @@
-./objects/tremo_lpuart.o: lib\src\tremo_lpuart.c \
-  C:\Keil_v535\ARM\ARMCLANG\include\stdio.h \
-  C:\Keil_v535\ARM\ARMCLANG\include\stdbool.h \
-  C:\Keil_v535\ARM\ARMCLANG\include\string.h lib\inc\tremo_rcc.h \
-  C:\Keil_v535\ARM\ARMCLANG\include\stdint.h lib\inc\tremo_regs.h \
-  chip\system_cm4.h chip\tremo_cm4.h chip\core_cm4.h \
-  D:\MCU\ASR6601\Ra-08\tremo_testFW\chip\cmsis_version.h \
-  D:\MCU\ASR6601\Ra-08\tremo_testFW\chip\cmsis_compiler.h \
-  D:\MCU\ASR6601\Ra-08\tremo_testFW\chip\cmsis_armclang.h \
-  lib\inc\tremo_lpuart.h
+./objects/tremo_lpuart.o: lib\src\tremo_lpuart.c lib\inc\tremo_rcc.h \
+  lib\inc\tremo_regs.h chip\system_cm4.h chip\tremo_cm4.h \
+  chip\core_cm4.h lib\inc\tremo_lpuart.h
--- Ra-08/tremo_testFW/Objects/tremo_timer.d
+++ Ra-08/tremo_testFW/Objects/tremo_timer.d
@@ -1,9 +1,3 @@
-./objects/tremo_timer.o: lib\src\tremo_timer.c \
-  C:\Keil_v535\ARM\ARMCLANG\include\string.h lib\inc\tremo_rcc.h \
-  C:\Keil_v535\ARM\ARMCLANG\include\stdint.h \
-  C:\Keil_v535\ARM\ARMCLANG\include\stdbool.h lib\inc\tremo_regs.h \
-  chip\system_cm4.h chip\tremo_cm4.h chip\core_cm4.h \
-  D:\MCU\ASR6601\Ra-08\tremo_testFW\chip\cmsis_version.h \
-  D:\MCU\ASR6601\Ra-08\tremo_testFW\chip\cmsis_compiler.h \
-  D:\MCU\ASR6601\Ra-08\tremo_testFW\chip\cmsis_armclang.h \
-  lib\inc\tremo_timer.h
+./objects/tremo_timer.o: lib\src\tremo_timer.c lib\inc\tremo_rcc.h \
+  lib\inc\tremo_regs.h chip\system_cm4.h chip\tremo_cm4.h \
+  chip\core_cm4.h lib\inc\tremo_timer.h

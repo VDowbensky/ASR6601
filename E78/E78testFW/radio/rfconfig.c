6ce85d1758e6e103f2931d5b6226e34d11547512
VDowbensky <harbinger@ukr.net>
2024-09-20 05:52:45 +0300

    2

--- E78/E78testFW/radio/rfconfig.c
+++ E78/E78testFW/radio/rfconfig.c
@@ -22,7 +22,6 @@ void radio_txconfig(void)
 		SX126X_SetTxParams(); //config power
 	}
 
-	
   else if (txconfig.modem == SX126X_MODEM_FSK) 
 	{
 		SX126X_FskConfig();
@@ -44,37 +43,7 @@ void radio_txconfig(void)
   return 0;
 }
 
-double step;
-  step = (double)FXO / 33554432;
-((uint32_t)(freq / step));
 
-int8_t SX126X_LoRaConfig(void)
-{
-//2. Define the protocol (LoRa® or FSK) with the command SetPacketType(...)
-    SX126X_SetPacketType(SX126X_MODEM_LORA);
-//3. Define the RF frequency with the command SetRfFrequency(...)
-    Channel = radioConfig.Channel;
-    SX126X_SetChannel();
-//4. Define where the data will be stored inside the data buffer in Rx with the command SetBufferBaseAddress(...)
-    SX126X_SetBufferBaseAddress(0, 0x80);
-//5. Define the modulation parameter according to the chosen protocol with the command SetModulationParams(...)1
-    SX126X_SetLoRaModParams(radioConfig.LoRaSf,radioConfig.LoRaBw,radioConfig.LoRaCr);
-//6. Define the frame format to be used with the command SetPacketParams(...)
-    SX126X_SetLoRaPacketParams(radioConfig.LoRaPreLen, radioConfig.LoRaImplHeader,radioConfig.LoRaPayLen,radioConfig.LoRaCrcOn,radioConfig.LoRaInvertIQ);
-//7. Configure DIO and irq: use the command SetDioIrqParams(...) to select the IRQ RxDone and map this IRQ to a DIO (DIO1
-//or DIO2 or DIO3), set IRQ Timeout as well.
-
-    //SX126X_SetDioIrqParams(radioConfig.LoRaIRQmsk,radioConfig.LoRaDIO1msk,radioConfig.LoRaDIO2msk,radioConfig.LoRaDIO3msk);
-
-//8. Define Sync Word value: use the command WriteReg(...) to write the value of the register via direct register access.
-    SX126X_writeReg(SX126X_REG_LRSYNC_H,(radioConfig.LoRaSyncWord & 0xff00) >> 8);
-    SX126X_writeReg(SX126X_REG_LRSYNC_L,radioConfig.LoRaSyncWord & 0xff);
-//9. Set the circuit in reception mode: use the command SetRx(). Set the parameter to enable timeout or continuous mode
-//10. Wait for IRQ RxDone2 or Timeout: the chip will stay in Rx and look for a new packet if the continuous mode is selected
-//otherwise it will goes to STDBY_RC mode.
-//11. In case of the IRQ RxDone, check the status to ensure CRC is correct: use the command GetIrqStatus()
-    return 0;
-}
 
 void radio_rxconfig(void)
 {

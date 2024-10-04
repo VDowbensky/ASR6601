e4029eae5047a995081afb858aaf75c621732d39
VDowbensky <harbinger@ukr.net>
2024-10-04 16:31:05 +0300

    10

--- E78/E78testFW/src/app_cli.c
+++ E78/E78testFW/src/app_cli.c
@@ -205,6 +205,8 @@ CommandEntry_t commands[] =
     COMMAND_ENTRY("SET_EM", "w", cli_setem, ""),
 		
 		COMMAND_ENTRY("SET_PACONFIG", "ww", cli_setpaconfig, ""), //technological
+		
+		COMMAND_ENTRY(NULL, NULL, NULL, NULL),
   };
 
 
